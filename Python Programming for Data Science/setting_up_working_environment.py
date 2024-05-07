946026f7f29f4fe81e17382453f56a499416fd9b
Merve Karagöz <89805569+merve-karagoz@users.noreply.github.com>
2024-05-07 14:50:16 +0300

    Add files via upload

---
+++ Python Programming for Data Science/setting_up_working_environment.py
@@ -0,0 +1,113 @@
+# deneme ortamı
+
+# data structers
+# numbers(sayılar) int float complex
+# strings str
+# boolean true false bool
+# tuple
+# dictionary
+
+# numbers (sayılar): int, float, complex
+
+x = 5
+type(x)
+
+x = 5.5
+type(x)
+
+int(x)
+
+x = 2j + 1
+type(x)
+
+x = "hello world"
+type(x)
+
+True
+False
+
+5 == 5
+7 == 2
+
+type(True)
+type(False)
+type(5 == 5)
+type(7 == 5)
+
+x = ["apple", "samsung", "huaweı"]
+type(x)
+
+x[0].capitalize()
+
+
+x = "hello"
+x.replace("l", "k")
+
+x.capitalize()
+"hello".capitalize()
+
+
+x ={"name": "John", "age": 25}
+type(x)
+
+"age" in x
+"Name" in x
+
+print("hello world")
+print('hello world')
+"hello world"
+
+long_str = """type(True),
+type(False),
+type(5 == 5),
+type(7 == 5)"""
+
+"type(false)" in long_str
+len(long_str)
+len("code")
+
+"code".upper()
+'CODE'.lower()
+
+new_list = [3, 7, 9]
+type(new_list)
+new_list = ["apple", "samsung", "huaweı"]
+new_list = [3, 7, 9, "apple", "samsung", "huaweı", True, [3, 7, 9]]
+new_list[6]
+new_list[7][2]
+new_list[3] = 5
+new_list[2] = "abc"
+new_list[0:3]
+
+dir(new_list)
+len(new_list)
+new_list.append(100)
+new_list.insert(9, "sed")
+
+dictionary = {"REG": "Regressıon",
+              "LOG": "Logaritma",
+              "YSA": ["Yapay, sinir, ağı"]}
+
+dictionary["REG"] = "ai"
+dictionary["YSA"][1]
+dictionary.values()
+
+
+name = "VBO_Bootcamp"
+
+type = "new_term"
+
+f"Name:{name} typpe{type}"
+
+total = 3.4 + 2.6
+
+print(total)
+
+a = 9 ** (1/2)
+
+b = 10 / 5
+
+a - b
+
+
+
