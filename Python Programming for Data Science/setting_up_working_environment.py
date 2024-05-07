2b721346bab7657146ab90900f5156a3067056f9
Merve Karagöz <89805569+merve-karagoz@users.noreply.github.com>
2024-05-07 14:52:40 +0300

    Update setting_up_working_environment.py

--- Python Programming for Data Science/setting_up_working_environment.py
+++ Python Programming for Data Science/setting_up_working_environment.py
@@ -1,113 +1,41 @@
-# deneme ortamı
+###############################################
+# Virtual Environment (Sanal Ortam)  ve (Package Managment) Paket Yönetimi
+###############################################
 
-# data structers
-# numbers(sayılar) int float complex
-# strings str
-# boolean true false bool
-# tuple
-# dictionary
+# Sanal ortamların listelenmesi:
+# conda env list
 
-# numbers (sayılar): int, float, complex
+# Sanal ortam oluşturma:
+# conda create –n myenv
 
-x = 5
-type(x)
+# Sanal ortamı aktif etme:
+# conda activate myenv
 
-x = 5.5
-type(x)
+# Yüklü paketlerin listelenmesi:
+# conda list
 
-int(x)
+# Paket yükleme:
+# conda install numpy
 
-x = 2j + 1
-type(x)
+# Aynı anda birden fazla paket yükleme:
+# conda install numpy scipy pandas
 
-x = "hello world"
-type(x)
+# Paket silme:
+# conda remove package_name
 
-True
-False
+# Belirli bir versiyona göre paket yükleme:
+# conda install numpy=1.20.1
 
-5 == 5
-7 == 2
+# Paket yükseltme:
+# conda upgrade numpy
 
-type(True)
-type(False)
-type(5 == 5)
-type(7 == 5)
+# Tüm paketlerin yükseltilmesi:
+# conda upgrade –all
 
-x = ["apple", "samsung", "huaweı"]
-type(x)
+# pip: pypi (python package index) paket yönetim aracı
 
-x[0].capitalize()
+# Paket yükleme:
+# pip install pandas
 
-
-x = "hello"
-x.replace("l", "k")
-
-x.capitalize()
-"hello".capitalize()
-
-
-x ={"name": "John", "age": 25}
-type(x)
-
-"age" in x
-"Name" in x
-
-print("hello world")
-print('hello world')
-"hello world"
-
-long_str = """type(True),
-type(False),
-type(5 == 5),
-type(7 == 5)"""
-
-"type(false)" in long_str
-len(long_str)
-len("code")
-
-"code".upper()
-'CODE'.lower()
-
-new_list = [3, 7, 9]
-type(new_list)
-new_list = ["apple", "samsung", "huaweı"]
-new_list = [3, 7, 9, "apple", "samsung", "huaweı", True, [3, 7, 9]]
-new_list[6]
-new_list[7][2]
-new_list[3] = 5
-new_list[2] = "abc"
-new_list[0:3]
-
-dir(new_list)
-len(new_list)
-new_list.append(100)
-new_list.insert(9, "sed")
-
-dictionary = {"REG": "Regressıon",
-              "LOG": "Logaritma",
-              "YSA": ["Yapay, sinir, ağı"]}
-
-dictionary["REG"] = "ai"
-dictionary["YSA"][1]
-dictionary.values()
-
-
-name = "VBO_Bootcamp"
-
-type = "new_term"
-
-f"Name:{name} typpe{type}"
-
-total = 3.4 + 2.6
-
-print(total)
-
-a = 9 ** (1/2)
-
-b = 10 / 5
-
-a - b
-
-
-
+# Paket yükleme versiyona göre:
+# pip install pandas==1.2.1
